894ce232921adfdc8d115c99ebae5052694aee20
Christopher Hernandez <cerberean661@gmail.com>
2019-09-10 16:42:36 -0700

    adding Action.js

---
+++ src/actions/Actions.js
@@ -0,0 +1,13 @@
+const removeItem = item => {
+    // dispatch an action here to remove an item
+    return {
+        type: "REMOVE_ITEM"
+    };
+  };
+
+  const buyItem = item => {
+    // dipsatch an action here to add an item
+    return {
+        type: "BUY_ITEM"
+    };
+  };
